0ec885ae0dcb5934b0fa636200dec72b2c5fde52
mayue-crrc <mayue.1881@163.com>
2018-05-29 08:17:45 +0800

    V1.16 modify subdoor signals

--- database.cpp
+++ database.cpp
@@ -82,7 +82,7 @@ Database::Database()
     HMiCT_ACDETestStartFlag_B1 = false;
 
     HMiCT_HMISWVerH_U8 = 1;
-    HMiCT_HMISWVerL_U8 = 15;
+    HMiCT_HMISWVerL_U8 = 16;
 
     for(int i =0; i< 12;i++)
     {
